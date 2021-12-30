bd5ca7d3b727401175066c1683fdbeef499a447b
Davi Bernardo <davi.santos@extreme.digital>
2021-12-30 15:57:05 -0300

    Incorporando expressões dentro do JSX

--- Introducao-ao-React/erjs/src/HelloWorld.tsx
+++ Introducao-ao-React/erjs/src/HelloWorld.tsx
@@ -1,5 +1,8 @@
 export default function HelloWorld () {
+
+    const name = 'Davi'
+
     return <div>
-        <h1>Hello, World!</h1>
+        <h1>Hello, { name === 'Davi' ? 'Davi' : name }!</h1>
     </div>
 }
