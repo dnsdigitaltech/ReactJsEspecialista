7b1097773dd1e41b8ffff055a4a61ef3d4025bce
Davi Bernardo <davi.santos@extreme.digital>
2021-12-30 16:32:34 -0300

    Como o JSX é Renderizado no DOM

--- Introducao-ao-React/erjs/src/App.tsx
+++ Introducao-ao-React/erjs/src/App.tsx
@@ -1,13 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
-import HelloWorld from './HelloWorld';
+import React from 'react';
+
+const Component = React.createElement('div', {style: {background: 'red'}}, null, 'Olá, mundo!')
+
+const HTMLElement = document.createElement('div')
+HTMLElement.style.background = 'red'
+HTMLElement.innerHTML = "Olá, mundo!"
+
+console.log(Component)
+console.log(HTMLElement)
+
+document.body.appendChild(HTMLElement)
+
+//@ts-ignore
+document.body.appendChild(Component)
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
 
-        <HelloWorld />
+        { Component }
 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
