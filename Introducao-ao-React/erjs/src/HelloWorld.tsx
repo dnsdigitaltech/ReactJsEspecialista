54f457f85759a45525a055d06536c68ad62b6f2b
Davi Bernardo <davi.santos@extreme.digital>
2021-12-30 16:09:12 -0300

    Atributos com JSX

--- Introducao-ao-React/erjs/src/HelloWorld.tsx
+++ Introducao-ao-React/erjs/src/HelloWorld.tsx
@@ -3,10 +3,11 @@ export default function HelloWorld () {
     const name = 'Davi'
 
     const title = <h1>Hello, { name }</h1>
+    const index = 0
 
     if (name === 'Davi') {
         return <div>
-            <h2 style={{ background: 'red' }}>{ name }</h2>
+            <h2 tabIndex={index} style={{ background: 'red' }}>{ name }</h2>
         </div>
     }
 
